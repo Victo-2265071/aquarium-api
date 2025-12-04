1278a05f4fc0fb8e6e1ec840ab41ee9262897304
WilliamChampagne <William_Champagne@hotmail.com>
2025-12-04 16:29:17 -0500

    ajout du path au config.ts

--- config.ts
+++ config.ts
@@ -1,18 +1,15 @@
 /* eslint-disable */
 import moduleAlias from 'module-alias';
+import path from 'path';
 
 // Check the env
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
-// // Configure "dotenv"
-// const result2 = dotenv.config({
-//   path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
-// });
-// if (result2.error) {
-//   throw result2.error;
-// }
-
 // Configure moduleAlias
 if (__filename.endsWith('js')) {
-  moduleAlias.addAlias('@src', __dirname + '/dist');
+  // Si on est dans dist/config.js, on remonte d'un niveau puis on va dans dist
+  const isInDist = __dirname.endsWith('dist');
+  const rootDir = isInDist ? path.join(__dirname, '..') : __dirname;
+
+  moduleAlias.addAlias('@src', path.join(rootDir, 'dist'));
 }
